091e5432223916923eb7d508886c3db9944a4a20
CalacasChidas <ale-jimenez@estudiantec.cr>
2023-03-02 00:30:27 -0600

    Parte gráfica

--- src/main/java/com/example/tareaextraclase1/HelloController.java
+++ src/main/java/com/example/tareaextraclase1/HelloController.java
@@ -1,14 +1,117 @@
 package com.example.tareaextraclase1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField Edad;
+
+    @FXML
+    private TextField Provincia;
+
+    @FXML
+    private TextField nombreIngresado;
+
+    @FXML
+    private Label resultado;
+
+    @FXML
+    void cartago(ActionEvent event) {
+        Provincia.setText("Cartago");
     }
+
+    @FXML
+    void alaj(ActionEvent event) {
+
+    }
+
+    @FXML
+    void divi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guana(ActionEvent event) {
+
+    }
+
+    @FXML
+    void here(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mimon(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nuevaPersona(ActionEvent event) {
+
+    }
+
+    @FXML
+    void persona11(ActionEvent event) {
+
+    }
+
+    @FXML
+    void persona21(ActionEvent event) {
+
+    }
+
+    @FXML
+    void persona22(ActionEvent event) {
+
+    }
+
+    @FXML
+    void persona31(ActionEvent event) {
+
+    }
+
+    @FXML
+    void persona32(ActionEvent event) {
+
+    }
+
+    @FXML
+    void persona41(ActionEvent event) {
+
+    }
+
+    @FXML
+    void persona42(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ponitsand(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resta(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sj(ActionEvent event) {
+
+    }
+
+    @FXML
+    void suma(ActionEvent event) {
+
+    }
+
 }
